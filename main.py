789bfea63320dc74e1585aadcf414c1a6a88172e
Sushmaravi011 <sushmaravi011@gmail.com>
2025-01-26 12:04:19 +0530

    Your commit message

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from playsound import *
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[1].id)
 
 def speak(audio):
     engine.say(audio)
@@ -51,7 +51,7 @@ def play_song(song_path):
 
 def play_music():
     speak("What song would you like me to play?") 
-    music_dir = r'S:\testingsong' 
+    music_dir = r'S:\Pattu_Song' 
     songs = os.listdir(music_dir)
     
     while True:
@@ -83,8 +83,65 @@ def play_music():
             break
         elif 'yes' not in query:
             speak("Sorry, I didn't catch that. Please say 'yes' to play another song or 'no' to stop.")
+def open_wikipedia(query):
+    speak("What topic would you like to search on Wikipedia?")
+    topic = take_command().lower()
 
+    if topic != "none":
+        speak(f"Searching Wikipedia for {topic}")
+        search_url = f"https://en.wikipedia.org/wiki/{topic.replace(' ', '_')}"
+        webbrowser.open(search_url)
+        speak(f"I've opened Wikipedia for {topic}.")
+    else:
+        speak("Sorry, I didn't catch that. Please try again.")
+            
+def open_youtube(query):
+    speak("What song would you like me to play?")
+    song_name = take_command().lower()
+    
+    if song_name != "none":
+        speak(f"Searching YouTube for {song_name}")
+        search_url = f"https://www.youtube.com/results?search_query={song_name}"
+        webbrowser.open(search_url)
+        speak("The search page is now open. Please say the number of the video you want to play (e.g., 'play 1', 'play 2').")
+        
+        while True:
+            video_number_query = take_command().lower()
+            video_number = None
+            for ordinal, num in range(1,5):
+                if ordinal in video_number_query:
+                    video_number = num
+                    break
+                if not video_number:
+                    video_number = 1
+                video_url = f"https://www.youtube.com/watch?v={video_number}"
+            try:
+                webbrowser.open(video_url)
+                speak(f"Now playing video {video_number}.")
+                break
+            except Exception as e:
+                speak("Sorry, I wasn't able to open the video. Please try again.")
+                print(f"Error: {e}")       
+        while True:
+            query = take_command().lower()
+            if 'yes' in query:
+                speak("Please say the number of the video you want to play.")
+                break
+            elif 'no' in query:
+                speak("Goodbye. Have a nice day!")
+                return
+            else:
+                speak("Sorry, I didn't catch that. Please say 'yes' to play another video or 'no' to stop.")
+    else:
+        speak("Sorry, I didn't catch that. Please try again.")
+def open_amazon():
+    speak("Opening Amazon.")
+    webbrowser.open("https://www.amazon.com")
 
+def open_flipkart():
+    speak("Opening Flipkart.")
+    webbrowser.open("https://www.flipkart.com")
+        
 music_dir = r'S:\testingsong'
 
 if __name__ == "__main__":
@@ -93,32 +150,26 @@ if __name__ == "__main__":
         query = take_command().lower()
 
         if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
-            query = query.replace('wikipedia', '')
-            results = wikipedia.summary(query, sentences=2)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
-
-        elif 'open youtube' in query:
-            speak("What song would you like me to play?")
-            song_name = take_command().lower()
-            if song_name != "none":
-                speak(f"Searching YouTube for {song_name}")
-                search_url = f"https://www.youtube.com/results?search_query={song_name}"
-                webbrowser.open(search_url)
-            else:
-                speak("Sorry, I didn't catch that. Please try again.")
-
+            open_wikipedia(query)
+            
         elif 'open google' in query:
             webbrowser.open("https://google.com")
 
         elif 'open stackoverflow' in query:
             webbrowser.open("https://stackoverflow.com")
+
+        elif 'play music' in query or 'play song' in query:
+            play_music()
+            
+        elif 'open youtube' in query:
+            open_youtube(query)
+            
+        elif 'amazon' in query:
+            open_amazon()
+
+        elif 'flipkart' in query:
+            open_flipkart()
             
         elif 'exit' in query:
             speak("Goodbye. Have a nice day!")
             break
-
-        elif 'play music' in query or 'play song' in query:
-            play_music() 
